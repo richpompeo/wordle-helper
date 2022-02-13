350c22a902aee8c425a73f4ea5f10894aa6ff7e6
jude253 <judecapachietti@gmail.com>
2022-02-13 13:30:31 -0500

    Add Buttons to UI

--- wordle-helper/src/components/Home.js
+++ wordle-helper/src/components/Home.js
@@ -5,6 +5,8 @@ const Home = () => {
     const [currentGuess, setCurrentGuess] = useState('');
     const [submittedGuess, setSubmittedGuess] = useState('');
     const [guesses, setGuesses] = useState([]);
+    const [previousGuesses, setPreviousGuesses] = useState([]);
+    const [previousDisplayWordLists, setPreviousDisplayWordLists] = useState([]);
     const [wordList, setWordList] = useState(allWords);
     const [displayWordList, setDisplayWordList] = useState([]);
 
@@ -12,11 +14,49 @@ const Home = () => {
     const [valueForRetainWordsWith, setValueForRetainWordsWith] = useState('');
     const [dataForRetainWordsWithLetterAt, setDataForRetainWordsWithLetterAt] = useState({ letter: '', index: 0 });
 
-    const [v0, setV0] = useState(0);
-    const [v1, setV1] = useState(0);
-    const [v2, setV2] = useState(0);
-    const [v3, setV3] = useState(0);
-    const [v4, setV4] = useState(0);
+
+    const buttonColors = ['grey', 'yellow', 'green']
+    const [b0, setB0] = useState(0);
+    const [b1, setB1] = useState(0);
+    const [b2, setB2] = useState(0);
+    const [b3, setB3] = useState(0);
+    const [b4, setB4] = useState(0);
+
+    const updateLetterColorButtonB0 = (event) => {
+        console.log('updateLetterColorButton', (b0 + 1) % 3);
+        event.preventDefault();
+        setB0((b0 + 1) % 3);
+    }
+    const updateLetterColorButtonB1 = (event) => {
+        console.log('updateLetterColorButton', (b1 + 1) % 3);
+        event.preventDefault();
+        setB1((b1 + 1) % 3);
+    }
+    const updateLetterColorButtonB2 = (event) => {
+        console.log('updateLetterColorButton', (b2 + 1) % 3);
+        event.preventDefault();
+        setB2((b2 + 1) % 3);
+    }
+    const updateLetterColorButtonB3 = (event) => {
+        console.log('updateLetterColorButton', (b3 + 1) % 3);
+        event.preventDefault();
+        setB3((b3 + 1) % 3);
+    }
+    const updateLetterColorButtonB4 = (event) => {
+        console.log('updateLetterColorButton', (b4 + 1) % 3);
+        event.preventDefault();
+        setB4((b4 + 1) % 3);
+    }
+
+    const resetButtonsAndGuess = () => {
+        console.log('resetButtonsAndGuess');
+        setB0(0);
+        setB1(0);
+        setB2(0);
+        setB3(0);
+        setB4(0);
+        setCurrentGuess('')
+    }
 
 
     const bigBrainFunction = (word, letterColors) => {
@@ -92,8 +132,12 @@ const Home = () => {
             console.log('handleSubmit', event);
             event.preventDefault();
             setSubmittedGuess(currentGuess);
-            wordListSynchronous = bigBrainFunction(currentGuess, [v0, v1, v2, v3, v4]);
-            setDisplayWordList(wordListSynchronous.map((word, index) => <Item value={word} key={index}/>))
+            setPreviousGuesses([...previousGuesses, [currentGuess, [b0, b1, b2, b3, b4]]]) // <-- to be displayed as previous guesses
+            wordListSynchronous = bigBrainFunction(currentGuess, [b0, b1, b2, b3, b4]);
+            setDisplayWordList(wordListSynchronous.map((word, index) => <Item value={word} key={index}/>));
+            setPreviousDisplayWordLists([...previousDisplayWordLists,wordListSynchronous]) // <-- for future "undo" button
+            
+            resetButtonsAndGuess();
             // if (currentGuess !== '') {
             //     setGuesses([...guesses, currentGuess])
             // }
@@ -151,7 +195,7 @@ const Home = () => {
         const retainWordsWithLetterAt = (list, letter, index) => {
             /**
              * Keep words in list that have letter at index. This function
-             *  is intended to handle plain "green" letters (as opposed to
+             * is intended to handle plain "green" letters (as opposed to
              * a word a that has duplicate letters, one of which is "green").
              * 
              * @param  {Object} list   List of remaining game words
@@ -169,10 +213,11 @@ const Home = () => {
             <div>
                 <p><b>Instructions:</b> After making a guess on wordle, type in your guess below.
                     <br></br>
-                    Then for each letter in your guess, enter in the color in the boxes below your guess
+                    Then for each letter in your guess, tap the button below so that the colors 
                     <br></br>
-                    (black = 0, yellow = 1, green = 2). Click submit to see the remaining wordle words! 
-                    </p>
+                    match what wordle shows. Click submit to see the remaining wordle words! 
+                </p>
+
                 <form
                     id="main-form"
                     onSubmit={e => e.preventDefault()}
@@ -189,54 +234,29 @@ const Home = () => {
                     </label>
                     {/* <input type="submit" value="Submit" /> */}
                     <button onClick={(e) => handleSubmit(e)}>submit guess</button>
+                    <br></br>
+                    Letter Colors:
+                    <button onClick={(e) => updateLetterColorButtonB0(e)} style={{backgroundColor: buttonColors[b0]}}>{currentGuess[0]}</button>
+                    <button onClick={(e) => updateLetterColorButtonB1(e)} style={{backgroundColor: buttonColors[b1]}}>{currentGuess[1]}</button>
+                    <button onClick={(e) => updateLetterColorButtonB2(e)} style={{backgroundColor: buttonColors[b2]}}>{currentGuess[2]}</button>
+                    <button onClick={(e) => updateLetterColorButtonB3(e)} style={{backgroundColor: buttonColors[b3]}}>{currentGuess[3]}</button>
+                    <button onClick={(e) => updateLetterColorButtonB4(e)} style={{backgroundColor: buttonColors[b4]}}>{currentGuess[4]}</button>
                     <br />
-                    Letter Color:
-                    <input
-                        style={{ maxWidth: 25 }}
-                        type="number"
-                        maxLength={1}
-                        value={v0}
-                        onChange={(event) => setV0(parseInt(event.target.value))}
-                    />
-                    <input
-                        style={{ maxWidth: 25 }}
-                        type="number"
-                        maxLength={1}
-                        value={v1}
-                        onChange={(event) => setV1(parseInt(event.target.value))}
-                    />
-                    <input
-                        style={{ maxWidth: 25 }}
-                        type="number"
-                        maxLength={1}
-                        value={v2}
-                        onChange={(event) => setV2(parseInt(event.target.value))}
-                    />
-                    <input
-                        style={{ maxWidth: 25 }}
-                        type="number"
-                        maxLength={1}
-                        value={v3}
-                        onChange={(event) => setV3(parseInt(event.target.value))}
-                    />
-                    <input
-                        style={{ maxWidth: 25 }}
-                        type="number"
-                        maxLength={1}
-                        value={v4}
-                        onChange={(event) => setV4(parseInt(event.target.value))}
-                    />
-                    <br />
-                    Letter Color Inputs: {[v0, v1, v2, v3, v4]}
+                    Letter Color Inputs: {[b0, b1, b2, b3, b4]}
                     <br />
                     Current Guess: {currentGuess}
                     <br />
                     Submitted Guess: {submittedGuess}
                     <br />
+                    Previous Guesses:
+                    <ul>
+                        {previousGuesses.map((word, index) => <Item value={word[0]} key={index}/>)}
+                    </ul>
                     <p>Number of 5 Letter Words Left: <b>{wordList.length}</b></p>
-                <ul>
-                    {displayWordList}
-                </ul>
+                    <p>Words Left:</p>
+                    <ul>
+                        {displayWordList}
+                    </ul>
                 </form>
             </div>
         )
